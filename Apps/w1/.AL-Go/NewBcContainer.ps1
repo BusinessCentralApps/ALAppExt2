1b1cac5c8e00099eb10ba3a5ae10e4571ec65102
freddydk <freddyk@microsoft.com>
2022-05-31 10:09:22 +0200

    dumps

--- Apps/w1/.AL-Go/NewBcContainer.ps1
+++ Apps/w1/.AL-Go/NewBcContainer.ps1
@@ -26,20 +26,20 @@ Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock
     if (-not (Test-Path $baseApp)) {
         $baseApp = "C:\Applications\BaseApp\Source\Microsoft_Base Application.app"
     }
-    Write-Host "Copying Base Application to packages path"
-    Copy-Item -Path $baseApp -Destination "c:\Run\my\Microsoft_Base Application.app"
+    Write-Host "Copying Base Application to my path"
+    Copy-Item -Path $baseApp -Destination "c:\run\my\Microsoft_Base Application.app"
 
     $application = "C:\Applications.*\Microsoft_Application_*.*.*.*.app"
     if (-not (Test-Path $application)) {
         $application = "C:\Applications\Application\Source\Microsoft_Application.app"
     }
-    Write-Host "Copying Application to packages path"
-    Copy-Item -Path $application -Destination "c:\Run\my\Microsoft_Application.app"
+    Write-Host "Copying Application to my path"
+    Copy-Item -Path $application -Destination "c:\run\my\Microsoft_Application.app"
 
     $testLibrariesApp = "C:\Applications.*\Microsoft_Tests-TestLibraries_*.*.*.*.app"
     if (-not (Test-Path $testLibrariesApp)) {
         $testLibrariesApp = "C:\Applications\BaseApp\Test\Microsoft_Tests-TestLibraries.app"
     }
-    Write-Host "Copying Tests-TestLibraries to packages path"
-    Copy-Item -Path $testLibrariesApp -Destination "c:\Run\my\Microsoft_Tests-TestLibraries.app"
+    Write-Host "Copying Tests-TestLibraries to my path"
+    Copy-Item -Path $testLibrariesApp -Destination "c:\run\my\Microsoft_Tests-TestLibraries.app"
 }
--- Apps/w1/.AL-Go/PublishBcContainerApp.ps1
+++ Apps/w1/.AL-Go/PublishBcContainerApp.ps1
@@ -8,14 +8,17 @@ $filename = [System.IO.Path]::GetFileName($parameters.appFile)
 if ($filename -like "Microsoft_System Application_*.*.*.*.app") {
     Write-Host "Publishing Base Application"
     $parameters.appFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\Microsoft_Base Application.app"
+    Get-Item $parameters.appFile | Out-Host
     Publish-BcContainerApp @parameters
 
     Write-Host "Publishing Application"
     $parameters.appFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\Microsoft_Application.app"
+    Get-Item $parameters.appFile | Out-Host
     Publish-BcContainerApp @parameters
 }
 elseif ($filename -like "Microsoft_System Application Test_*.*.*.*.app") {
     Write-Host "Publishing Tests-TestLibraries"
     $parameters.appFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\Microsoft_Tests-TestLibraries.app"
+    Get-Item $parameters.appFile | Out-Host
     Publish-BcContainerApp @parameters
 }
