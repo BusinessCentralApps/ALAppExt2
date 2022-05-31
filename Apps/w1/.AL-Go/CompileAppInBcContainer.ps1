7b8a63f40b80003d7f461b88a80b132216cfb932
freddydk <freddyk@microsoft.com>
2022-05-31 10:46:36 +0200

    inside

--- Apps/w1/.AL-Go/CompileAppInBcContainer.ps1
+++ Apps/w1/.AL-Go/CompileAppInBcContainer.ps1
@@ -3,10 +3,10 @@ Param(
 )
 
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { Param([string]$packagesFolder)
-    if (!(Test-Path -Path $packagesFolder -PathType Container)) {
-        New-Item -Path $packagesFolder -ItemType Directory | Out-Null
-    }
     if (!(Test-Path (Join-Path $packagesFolder "Microsoft_Application.app"))) {
+        if (!(Test-Path -Path $packagesFolder -PathType Container)) {
+            New-Item -Path $packagesFolder -ItemType Directory | Out-Null
+        }
         Write-Host "Copying apps to packages folder"
         Copy-Item -path "c:\run\my\Microsoft_*.app" -Destination $packagesFolder
     }
