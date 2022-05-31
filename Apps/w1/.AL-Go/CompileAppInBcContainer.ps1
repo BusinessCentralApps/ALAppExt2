24daca287d92dc4f2edf3527b9a0f45b29ff5101
freddydk <freddyk@microsoft.com>
2022-05-31 10:24:46 +0200

    copy apps in place

---
+++ Apps/w1/.AL-Go/CompileAppInBcContainer.ps1
@@ -0,0 +1,13 @@
+Param(
+    [Hashtable] $parameters
+)
+
+Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { Param([string]$packagesFolder)
+    if (!(Test-Path (Join-Path $packagesFolder "Microsoft_Application.app"))) {
+        Write-Host "Copying apps to packages folder"
+        Copy-Item -path "c:\run\my\Microsoft_*.app" -Destination $packagesFolder
+    }
+} -argumentList (Get-BcContainerPath -ContainerName $parameters.ContainerName -path $Parameters.appSymbolsFolder) | Out-Null
+
+Compile-AppInBcContainer @parameters
+
