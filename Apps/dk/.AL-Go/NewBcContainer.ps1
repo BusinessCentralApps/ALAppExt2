e65fd87ff044281e2eaa5fc79ba86ffcbc9d5a12
freddydk <freddyk@microsoft.com>
2022-06-02 16:14:53 +0200

    remove star

--- Apps/dk/.AL-Go/NewBcContainer.ps1
+++ Apps/dk/.AL-Go/NewBcContainer.ps1
@@ -14,7 +14,7 @@ New-BcContainer @parameters -dumpEventLog
 
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 $installedApps | ForEach-Object {
-    $removeData = $_.Name -ne "Base Application*"
+    $removeData = $_.Name -ne "Base Application"
     Write-Host "Removing $($_.Name)"
     Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData:$removeData -doNotSaveSchema:$removeData -force
 }
